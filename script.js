c70e6799b37b40b066d192cc0b875f731597c05d
Chase Theriot <chasetheriot@Chases-Mac-mini.local>
2026-07-04 11:20:02 -0500

    first website

---
+++ script.js
@@ -0,0 +1,63 @@
+// Accordion
+
+const buttons=document.querySelectorAll(".accordion-btn");
+
+buttons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+const content=button.nextElementSibling;
+
+document.querySelectorAll(".accordion-content").forEach(item=>{
+
+if(item!==content){
+
+item.style.maxHeight=null;
+
+item.classList.remove("open");
+
+}
+
+});
+
+if(content.classList.contains("open")){
+
+content.style.maxHeight=null;
+
+content.classList.remove("open");
+
+}else{
+
+content.classList.add("open");
+
+content.style.maxHeight=content.scrollHeight+"px";
+
+}
+
+});
+
+});
+
+
+
+// Fade In Animation
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},{threshold:.15});
+
+document.querySelectorAll(".fade-in").forEach(el=>{
+
+observer.observe(el);
+
+});
