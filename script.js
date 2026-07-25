d24eb1d91775b5ddc4df2aa206768db9f1ff7226
Chase Theriot <lakehighlandslandscaping@gmail.com>
2026-07-25 15:47:50 -0500

    Mobile/Header Fixes

--- script.js
+++ script.js
@@ -61,3 +61,17 @@ document.querySelectorAll(".fade-in").forEach(el=>{
 observer.observe(el);
 
 });
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+if(menuToggle){
+
+menuToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("show");
+
+});
+
+}
+
